578be35e495f58ff6524b862b9b6118425799fe5
luis <l.miguel0529@hotmaail.com>
2020-09-21 13:15:16 -0500

    Add all folder

---
+++ src/main/java/co/com/konecta/techincaltest/KonectatechincaltestApplication.java
@@ -0,0 +1,13 @@
+package co.com.konecta.techincaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonectatechincaltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonectatechincaltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/konecta/techincaltest/KonectatechincaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.konecta.techincaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonectatechincaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
